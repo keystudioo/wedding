6db13f9ea84b62f2f31db01127511dd4c15feb18
Katarina Bagaric <92014304+keystudioo@users.noreply.github.com>
2026-03-29 23:40:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const weddingData = {
   couple: "Helena & Ante",
   heroImage: "/helena-ante.jpeg",
   introVideo: "/pressed-love-envelope-52d49bf5.mp4",
-  introPoster: "/intro-poster.jpeg",
+  introPoster: "/intro-poster.jpg",
   musicFile: "/wedding-song.mp3",
   weddingDate: "2026-09-04T17:30:00",
   message:
